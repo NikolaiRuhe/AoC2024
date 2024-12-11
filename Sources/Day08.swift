df26113cc03f697fd134d2cd25ff74958c628955
Nikolai Ruhe <git@nikolai-ruhe.de>
2024-12-11 15:48:50 +0100

    Day08

--- Sources/Day08.swift
+++ Sources/Day08.swift
@@ -1,18 +1,93 @@
 import Algorithms
 
-// https://adventofcode.com/2023/day/8
+/// Resonant Collinearity
+/// https://adventofcode.com/2024/day/8
 struct Day08: AdventDay {
   static let testData = """
+    ............
+    ........0...
+    .....0......
+    .......0....
+    ....0.......
+    ......A.....
+    ............
+    ............
+    ........A...
+    .........A..
+    ............
+    ............
     """
 
-  static var expectedTestResult1:   String { "unknown" }
-  static var expectedProperResult1: String { "unknown" }
-  static var expectedTestResult2:   String { "unknown" }
-  static var expectedProperResult2: String { "unknown" }
+  static var expectedTestResult1:   String { "14" }
+  static var expectedProperResult1: String { "249" }
+  static var expectedTestResult2:   String { "34" }
+  static var expectedProperResult2: String { "905" }
 
-  var data: String
-  init(data: String) { self.data = data }
+  var antennas: [Character: [Coord]] = [:]
+  var width: Int
+  var height: Int
 
-  func part1() -> Any { "not implemented" }
-  func part2() -> Any { "not implemented" }
+  init(data: String) {
+    let lines = data.split(separator: "\n")
+    self.width = lines[0].count
+    self.height = lines.count
+
+    for (row, line) in lines.enumerated() {
+      for (col, char) in line.enumerated() where char != "." {
+        antennas[char, default: []].append(Coord(row: row, col: col))
+      }
+    }
+  }
+
+  var coordinatePairs: some Sequence<(Coord, Coord)> {
+    antennas
+      .values
+      .flatMap {
+        $0.combinations(ofCount: 2).map { ($0[0], $0[1]) }
+      }
+  }
+
+  func isInRange(_ coord: Coord) -> Bool {
+    return coord.row >= 0 && coord.row < height && coord.col >= 0 && coord.col < width
+  }
+
+  func part1() -> Any {
+    var result: Set<Coord> = []
+    func insert(_ coord: Coord) { if isInRange(coord) { result.insert(coord) } }
+
+    for (lhs, rhs) in coordinatePairs {
+      let distance = lhs - rhs
+      insert(lhs + distance)
+      insert(rhs - distance)
+    }
+
+    return result.count
+  }
+
+  func part2() -> Any {
+    var result: Set<Coord> = []
+
+    for (lhs, rhs) in coordinatePairs {
+      let distance = lhs - rhs
+      var pos = lhs
+      while isInRange(pos) { result.insert(pos); pos = pos + distance }
+      pos = lhs - distance
+      while isInRange(pos) { result.insert(pos); pos = pos - distance }
+    }
+
+    return result.count
+  }
+
+  struct Coord: Hashable {
+    var row: Int
+    var col: Int
+  }
+}
+
+func + (lhs: Day08.Coord, rhs: Day08.Coord) -> Day08.Coord {
+  .init(row: lhs.row + rhs.row, col: lhs.col + rhs.col)
+}
+
+func - (lhs: Day08.Coord, rhs: Day08.Coord) -> Day08.Coord {
+  .init(row: lhs.row - rhs.row, col: lhs.col - rhs.col)
 }
